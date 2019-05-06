1cc1a1e7d9aa7535cf8a25c2ffde0933f8f43312
Rob McMahon <robmcmahon@Michaels-iMac.local>
2019-05-06 16:58:57 -0400

    Adding datasource and delegate for tableViews

--- Makerspace/Makerspace/Controllers/HomeVC.swift
+++ Makerspace/Makerspace/Controllers/HomeVC.swift
@@ -11,6 +11,17 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    @IBOutlet weak var signInTableView: UITableView!
+    @IBOutlet weak var signOutTableView: UITableView!
+    
+    @IBAction func singInButtonTouched(_ sender: CustomButton) {
+        
+    }
+    
+    @IBAction func signOutButtonTouched(_ sender: CustomButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -23,4 +34,33 @@ class HomeVC: UIViewController {
         }
     }
     
-} //end class 
+} //end class
+
+extension HomeVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == signInTableView {
+            return UserManager.instance.users.count // -active users
+        }
+        else {
+            return /*active users - */ UserManager.instance.users.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == signInTableView {
+            guard let cell = signInTableView.dequeueReusableCell(withIdentifier: "signinCell") else {
+                preconditionFailure("Check reuse ID")
+            }
+        }
+        else {
+            guard let cell = signOutTableView.dequeueReusableCell(withIdentifier: "signoutCell") else {
+                preconditionFailure("Check reuse ID")
+            }
+        }
+        //blah blah cell
+    }
+    return cell
+    
+    
+    
+} //end extension
