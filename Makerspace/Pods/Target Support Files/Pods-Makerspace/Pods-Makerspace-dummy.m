4fbf92b01ecd0d223ed367ec8600e50b42b06ca9
Rob McMahon <robmcmahon@Michaels-iMac.local>
2019-05-06 11:48:24 -0400

    Adding podfiles

---
+++ Makerspace/Pods/Target Support Files/Pods-Makerspace/Pods-Makerspace-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Makerspace : NSObject
+@end
+@implementation PodsDummy_Pods_Makerspace
+@end
---
+++ Makerspace/Pods/Target Support Files/Pods-MakerspaceTests/Pods-MakerspaceTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MakerspaceTests : NSObject
+@end
+@implementation PodsDummy_Pods_MakerspaceTests
+@end
