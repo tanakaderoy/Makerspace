c2fa4285e979adaf42dadba8c5a983c16b4ef89d
robbymcm <robert.mcmahon@otterbein.edu>
2019-05-09 16:46:25 -0400

    Adding viewcontrollers to storyboard and VC folder for admin sign in and user creation

---
+++ Makerspace/Makerspace/Controllers/Admin/CreateUserVC.swift
@@ -0,0 +1,15 @@
+//
+//  CreateUserVC.swift
+//  Makerspace
+//
+//  Created by Rob McMahon on 5/9/19.
+//  Copyright © 2019 Rob McMahon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateUserVC: UIViewController {
+    
+    
+} //end class
---
+++ Makerspace/Makerspace/Controllers/Admin/LoginVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoginVC.swift
+//  Makerspace
+//
+//  Created by Rob McMahon on 5/9/19.
+//  Copyright © 2019 Rob McMahon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+} //end class
