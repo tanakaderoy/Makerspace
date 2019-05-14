f323d011ec502d107050ff16ae29f8661350b34e
robbymcm <robert.mcmahon@otterbein.edu>
2019-05-13 21:54:39 -0400

    Fixed labels in firebase. Found a bug, user and unique user counts do not save after app is closed.

--- Makerspace/Makerspace/Models/Room/RoomNetworkAdaptor.swift
+++ Makerspace/Makerspace/Models/Room/RoomNetworkAdaptor.swift
@@ -17,7 +17,7 @@ class RoomNetworkAdaptor {
     
     //updates total users for the month, or until cleared
     func updateUsers(room: Room) {
-        let values = ["totalUsers" : room.totalUsers!, "uniqueUsers" : room.uniqueUsers] as [String : Any]
+        let values = ["Total Users" : room.totalUsers!, "Unique Users" : room.uniqueUsers] as [String : Any]
         db.collection("rooms").document(room.roomName).setData(values as [String : Any])
     }
 } //end class
